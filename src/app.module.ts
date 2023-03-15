a1e183829d0bc784f9e4f50bd017682450080230
aminelahrim <aminelahrimdev@gmail.com>
2023-03-15 10:38:58 +0100

    add many to many relations; GET, CREATE for child API; transform for many to many for JSON response;

---
+++ src/app.module.ts
@@ -0,0 +1,69 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ImprovementNeedModule } from './improvement-need/improvement-need.module';
+import * as path from 'path';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import { NeuroDiverseConditionModule } from './neuro-diverse-condition/neuro-diverse-condition.module';
+import { UserModule } from './user/user.module';
+import { ChildModule } from './child/child.module';
+import { CodeModule } from './code/code.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: false,
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        //process.env what you have in CMD
+        const synchronize = process.env.STAGE != 'prod';
+
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB'),
+          autoLoadEntities: true,
+          synchronize: synchronize,
+        };
+      },
+    }),
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      fallbacks: {
+        'en-*': 'en',
+        'fr-*': 'fr',
+      },
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
+    }),
+    ImprovementNeedModule,
+    UserModule,
+    NeuroDiverseConditionModule,
+    ChildModule,
+    CodeModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { TransformInterceptor } from './transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    snapshot: true,
+  });
+  _initConfig(app);
+
+  _initSwagger(app);
+  const port = process.env.STAGE == 'prod' ? process.env.PORT : 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+
+function _initConfig(app: NestExpressApplication) {
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors();
+}
+
+function _initSwagger(app: NestExpressApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('SenLife')
+    .setDescription('The SenLife API description')
+    .setVersion('1.0')
+    .addTag('SenLife')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
+bootstrap();
