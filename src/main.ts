8cb210a31a7900c88ff67c803055b5067b354af8
aminelahrim <aminelahrimdev@gmail.com>
2023-03-15 17:07:12 +0100

    Fix PORT

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   _initSwagger(app);
   logger.log(`process.env.ENV=${process.env.ENV}`);
-  const port = process.env.ENV == 'dev' ? 3000 : process.env.$PORT;
+  const port = process.env.ENV == 'dev' ? 3000 : process.env.PORT;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
