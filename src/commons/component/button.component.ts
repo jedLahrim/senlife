a6b6f0e624c26518fc57c8ecb6a00c7370f43f53
jed <jolix123@gmail.com>
2023-03-21 15:28:35 +0000

    add sending mails with aws SES service in pr

---
+++ src/commons/component/button.component.ts
@@ -0,0 +1,18 @@
+export class ButtonComponent {
+  // ACTIVATE BUTTON
+  static ACTIVATE_NOW = (url) => {
+    return `<p>Hello,</p>
+    <p>Please click on the following button to activate your account address:</p>
+    <a href="${url}">
+      <button style="background-color: #008CBA; color: white; padding: 12px 24px; border: none; border-radius: 5px; cursor: pointer;">Activate Now</button>
+    </a>`;
+  };
+  // ACTIVATE BUTTON
+  static VERIFY_LOGIN = (url) => {
+    return `<p>Hello,</p>
+    <p>Please click on the following button to verify your email address:</p>
+    <a href="${url}">
+       <button style="background-color: #008CBA; color: white; padding: 12px 24px; border: none; border-radius: 5px; cursor: pointer;">Verify Login</button>
+    </a>`;
+  };
+}
