b742ed838bdfbce79bb1507bf9cd70fb16e2e34d
jed <jolix123@gmail.com>
2023-03-21 15:11:35 +0000

    add sending mails with aws SES service in pr

--- src/commons/constant.ts
+++ src/commons/constant.ts
@@ -1,3 +1,5 @@
+import { ButtonComponent } from './component/button.component';
+
 export class Constant {
   static randomCodeString = function (length = 6) {
     return Math.random().toString(20).substr(2, length);
@@ -6,18 +8,10 @@ export class Constant {
   static accessExpireIn = 86400000;
   static refreshExpireIn = 172800000;
   static activateHtml = function (url: string) {
-    return `<p>Hello,</p>
-    <p>Please click on the following button to activate your account address:</p>
-    <a href="${url}">
-      <button>Activate Now</button>
-    </a>`;
+    return ButtonComponent.ACTIVATE_NOW(url);
   };
   static verifyLoginHtml = function (url: string) {
-    return `<p>Hello,</p>
-    <p>Please click on the following button to verify your email address:</p>
-    <a href="${url}">
-      <button>Verify Login</button>
-    </a>`;
+    return ButtonComponent.VERIFY_LOGIN(url);
   };
 
   static ANDROID_PACKAGE_NAME = 'com.senlife.app';
